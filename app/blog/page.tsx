a7aa605d2cc0adb5f19f7de147ec465885322c34
Aryan Singh <aryan.aryansingh.singh008@gmail.com>
2024-12-06 14:51:06 +0530

    updated blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,11 @@
 import FooterMenu from "@/components/home-page/home-6/FooterMenu";
 import CopyrightFooter2 from "@/components/footer/CopyrightFooter2";
 // import Pagination from "@/components/blog/Pagination";
-import SearchBox from "@/components/blog/SearchBox";
-import Category from "@/components/blog/Category";
-import RecentPost from "@/components/blog/RecentPost";
+// import SearchBox from "@/components/blog/SearchBox";
+// import Category from "@/components/blog/Category";
+// import RecentPost from "@/components/blog/RecentPost";
 // import BannerPost from "@/components/blog/BannerPost";
-import Blog2 from "@/components/blog/Blog";
+// import Blog2 from "@/components/blog/Blog";
 import Header from "@/components/home-page/home-6/Header";
 import Image from "next/image";
 
@@ -30,21 +30,25 @@ const GridWithSidebar = () => {
             <div className="col-lg-7" data-aos="fade-right">
               <div className="title-style-five mb-65 lg-mb-40">
                 
-                <h2 className="main-title fw-500 tx-dark">
-                  Check our inside news &amp; update.
+                <h2 style={{textAlign:'center',
+                  justifyContent:'center',
+                  
+                  
+                }}className="main-title fw-500 tx-dark">
+                  Latest Blogs
                 </h2>
               </div>
             </div>
           </div>
         </div>
         {/* /.container */}
-        <Image
+        {/* <Image
           src="/images/shape/shape_172.svg"
           alt="shape"
           height={280}
           width={360}
           className="lazy-img shapes shape-two"
-        />
+        /> */}
       </div>
 
       {/*
@@ -52,51 +56,51 @@ const GridWithSidebar = () => {
 				Blog Section Five
 			=====================================================
 			*/}
-      <div className="blog-section-five mt-70 lg-mt-30">
+      {/* <div className="blog-section-five mt-70 lg-mt-30">
         <div className="container">
           <div className="border-bottom pb-130 lg-pb-60">
             <div className="row gx-xl-5">
               <div className="col-lg-8">
                 <div className="blog-meta-wrapper pe-xxl-5">
-                  <div className="row">
-                    <Blog2 />
-                  </div>
-                </div>
+                  <div className="row"> */}
+                    {/* <Blog2 /> */}
+                  {/* </div>
+                </div> */}
                 {/* /.blog-meta-wrapper */}
                 {/* <div className="page-pagination-one pt-15">
                   <Pagination />
                 </div> */}
-              </div>
+              {/* </div> */}
               {/* End .col-lg-8 */}
 
-              <div className="col-lg-4 col-md-8">
-                <div className="blog-sidebar md-mt-70">
-                  <div className="blog-sidebar-search mb-55 md-mb-40">
+              {/* <div className="col-lg-4 col-md-8">
+                <div className="blog-sidebar md-mt-70"> */}
+                  {/* <div className="blog-sidebar-search mb-55 md-mb-40">
                     <SearchBox />
-                  </div>
+                  </div> */}
                   {/* /.blog-sidebar-search */}
 
-                  <div className="blog-sidebar-category mb-60 md-mb-50">
+                  {/* <div className="blog-sidebar-category mb-60 md-mb-50">
                     <h4 className="sidebar-title">Category</h4>
                     <Category />
-                  </div>
+                  </div> */}
                   {/* /.blog-sidebar-category */}
 
-                  <div className="sidebar-recent-news mb-60 md-mb-50">
+                  {/* <div className="sidebar-recent-news mb-60 md-mb-50">
                     <h4 className="sidebar-title">Recent News</h4>
                     <RecentPost />
-                  </div>
+                  </div> */}
                   {/* /.sidebar-recent-news */}
 
                   {/* <BannerPost /> */}
                   {/* /.sidebar-banner-add */}
-                </div>
+                {/* </div> */}
                 {/* /.blog-sidebar */}
-              </div>
+              {/* </div>
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* Footer */}
       <div className="footer-style-two theme-basic-footer">
